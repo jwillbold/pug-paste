e5c68b11f31e84ff0fe7012d9f7516750f99423c
Johannes Willbold <johannes.willbold@rub.de>
2019-09-18 13:46:24 +0200

    Added fragmenting

--- spec/pug-paste-spec.js
+++ spec/pug-paste-spec.js
@@ -11,27 +11,46 @@ const regularText = '\
 Some regular text\n\
 With multiple lines\n';
 
-const htmlText = '\
-<!doctype html>\n\
-<html lang="en">\n\
-  <head>\n\
-    <title>Hello World!</title>\n\
-  </head>\n\
-  <body>\n\
-    <div id="content">\n\
-      <h1 class="title">Hello World!</h1>\n\
-    </div>\n\
-  </body>\n\
-</html>';
+// Currenlty head, body and html tags are ignored. Keep these two for later.
+// const htmlText = '\
+// <!doctype html>\n\
+// <html lang="en">\n\
+// \t<head>\n\
+// \t\t<title>An HTML site</title>\n\
+// \t</head>\n\
+// \t<body>\n\
+// \t\t<div class="container">\n\
+// \t\t\t<h1 id="title">Hello World!</h1>\n\
+// \t\t</div>\n\
+// \t</body>\n\
+// </html>';
 
-const pugText = "\
-doctype html\n\
-html(lang='en')\n\
-\thead\n\
-\t\ttitle Hello World!\n\
-\tbody\n\
-\t\t#content\n\
-\t\t\th1.title Hello World!";
+// const pugText = "\
+// doctype html\n\
+// html(lang='en')\n\
+// \thead\n\
+// \t\ttitle An HTML site\n\
+// \tbody\n\
+// \t\t.container\n\
+// \t\t\th1#title Hello World!";
+
+const htmlFragmentText ='\
+<div class="container">\n\
+\t<ul class="nav">\n\
+\t\t<li class="nav-item active"><a href="?lang=en"><i>EN</i></a></li>\n\
+\t</ul>\n\
+\t<h1 id="title"> Hello World</h1>\n\
+\t<p> Welcome to my site </p>\n\
+</div>';
+
+const pugFragmentText = "\
+.container\n\
+\tul.nav\n\
+\t\tli.nav-item.active\n\
+\t\t\ta(href='?lang=en')\n\
+\t\t\t\ti EN\n\
+\th1#title Hello World\n\
+\tp Welcome to my site";
 
 const commandEnableclipboardTransform = "pug-paste:enable-clipboard-transform";
 const commandDisableclipboardTransform = "pug-paste:disable-clipboard-transform";
@@ -67,12 +86,12 @@ describe('PugPaste', () => {
         }
       };
 
-      it("paste html to test.html", testHelper('test.html', htmlText, htmlText));
-      it("paste html to test.pug", testHelper('test.pug', htmlText, pugText));
-      it("paste html to test.txt", testHelper('test.txt', htmlText, htmlText));
+      it("paste html to test.html", testHelper('test.html', htmlFragmentText, htmlFragmentText));
+      it("paste html to test.pug", testHelper('test.pug', htmlFragmentText, pugFragmentText));
+      it("paste html to test.txt", testHelper('test.txt', htmlFragmentText, htmlFragmentText));
 
       it("paste text to test.pug", testHelper('test.pug', regularText, regularText));
-      it("paste pug to test.pug", testHelper('test.pug', pugText, pugText));
+      it("paste pug to test.pug", testHelper('test.pug', pugFragmentText, pugFragmentText));
     };
   };
 
